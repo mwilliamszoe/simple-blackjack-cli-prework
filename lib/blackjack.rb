8a70cd8e9212ee8f29cbde098ddf56e941ed8499
Zoe Monet Williams <mwilliamszoe@gmail.com>
2018-08-20 20:02:01 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,62 +1,62 @@
 def welcome
-  # code #welcome here
   puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-rand(1..11)
+  card = rand(1..11) #reuse the randomize method by setting it equal to a variable
 end
 
 def display_card_total(card_total)
-  # code #display_card_total here
   puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
   puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
   user_input = gets.chomp
 end
 
 def end_game(card_total)
-  # code #end_game here
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
- puts "#{deal_card} + #{deal_card}"
-end
-
-def hit?(n)
-  # code hit? here
-  prompt_user
-  choice = get_user_input
-  if choice = 'h'
-    deal_card
-  elsif choice = 's'
-    nil
-  else choice != 'h' && choice != 's'
-    invalid_command
-    prompt_user
-  end
+  card_one = deal_card
+  card_two = deal_card
+  card_total = card_one + card_two
+  display_card_total(card_total)
   card_total
 end
 
-def invalid_command
-  # code invalid_command here
-  puts "Please enter a valid command."
+def hit?(card_total)
+  prompt_user
+  user_input = get_user_input #set a variable to use for your if/else statement
+  if user_input == "h"
+    card_total += deal_card 
+  elsif user_input == "s"
+    card_total 
+  else
+    invalid_command
+  end
 end
 
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
+def invalid_command
+  puts "Please enter a valid command"
+  prompt_user
+  get_user_input
+end
+
 
 def runner
-  # code runner here
+  welcome
+  sum = initial_round
+  until sum > 21 
+    sum = hit?(sum)
+    display_card_total(sum)
+  end
+  end_game(sum)
 end
-    
+
+runner 
